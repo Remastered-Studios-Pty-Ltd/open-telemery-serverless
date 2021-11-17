f80ea4a939e654ce29563df21d32507fdcd5c84f
Melcom van Eeden <melcomve@outlook.com>
2021-11-17 02:50:23 +0200

    readme update

--- otel-collector/ec2/cloudformation-create.sh
+++
@@ -1,8 +0,0 @@
-# IAMRole=AWSDistroOpenTelemetryRole
-# ParameterKey=IAMRole,ParameterValue=${IAMRole}
-
-aws cloudformation create-stack --stack-name sts-open-telemetry-collector-dev \
-    --template-body file://aws-otel-ec2-deployment-cfn.yaml \
-    --parameters ParameterKey=KeyName,ParameterValue="$AWS_EC2_SSH_KEY_NAME" \
-    --capabilities CAPABILITY_NAMED_IAM \
-    --region "${AWS_REGION}"
