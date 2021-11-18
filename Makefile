5237083ea7a31d91ec1fe82693fb295df25462ea
Melcom van Eeden <melcomve@outlook.com>
2021-11-18 18:45:53 +0200

    Makefile update

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 .PHONY: build clean deploy
 
 build:
-	env GOARCH=amd64 GOOS=linux go build -ldflags="-s -w" -o bin/tracesgo/main structure/functions/capture/tracesgo/main.go
-	chmod 777 bin/tracesgo/main
+	env GOARCH=amd64 GOOS=linux go build -ldflags="-s -w" -o bin/tracesgo structure/functions/capture/tracesgo/main.go
+	chmod 777 bin/tracesgo
 
 clean:
 	rm -rf ./bin ./vendor Gopkg.lock
--- structure/functions/capture/tracesgo/main.go
+++ structure/functions/capture/tracesgo/main.go
@@ -4,7 +4,9 @@ import (
     "context"
     "fmt"
     "github.com/aws/aws-lambda-go/events"
+    openTelemetryTrace "github.com/StackVista/stackstate-agent/pkg/trace/pb/open-telemetry/trace/v1"
     runtime "github.com/aws/aws-lambda-go/lambda"
+    "github.com/gogo/protobuf/proto"
 )
 
 func main() {
@@ -12,8 +14,47 @@ func main() {
 }
 
 func Handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-    fmt.Printf(request.Body)
-    fmt.Printf("Code Change 2")
+    traceData := &openTelemetryTrace.TracesData{}
+    if err := proto.Unmarshal([]byte(request.Body), traceData); err != nil {
+        fmt.Printf("%v", err)
+    }
+    fmt.Printf("%v", traceData)
+
+    traceData2 := &openTelemetryTrace.ResourceSpans{}
+    if err := proto.Unmarshal([]byte(request.Body), traceData2); err != nil {
+        fmt.Printf("%v", err)
+    }
+    fmt.Printf("%v", traceData2)
+
+    traceData3 := &openTelemetryTrace.InstrumentationLibrarySpans{}
+    if err := proto.Unmarshal([]byte(request.Body), traceData3); err != nil {
+        fmt.Printf("%v\n", err)
+    }
+    fmt.Printf("%v\n\n", traceData3)
+
+    traceData4 := &openTelemetryTrace.Span{}
+    if err := proto.Unmarshal([]byte(request.Body), traceData4); err != nil {
+        fmt.Printf("%v\n", err)
+    }
+    fmt.Printf("%v\n\n", traceData4)
+
+    traceData5 := &openTelemetryTrace.Span_Event{}
+    if err := proto.Unmarshal([]byte(request.Body), traceData5); err != nil {
+        fmt.Printf("%v\n", err)
+    }
+    fmt.Printf("%v\n\n", traceData5)
+
+    traceData6 := &openTelemetryTrace.Span_Link{}
+    if err := proto.Unmarshal([]byte(request.Body), traceData6); err != nil {
+        fmt.Printf("%v\n", err)
+    }
+    fmt.Printf("%v\n\n", traceData6)
+
+    traceData7 := &openTelemetryTrace.Status{}
+    if err := proto.Unmarshal([]byte(request.Body), traceData7); err != nil {
+        fmt.Printf("%v\n", err)
+    }
+    fmt.Printf("%v\n\n", traceData7)
 
     // fmt.Printf("Processing request data for request %s.\n", request.RequestContext.RequestID)
     // fmt.Printf("Body size = %d.\n", len(request.Body))
